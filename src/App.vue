9975cf762a2f92836f556a89db6c3e6dc3a1aaa1
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-07-14 11:17:16 +0200

    debugging

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
       urlFilm:"https://api.themoviedb.org/3/search/movie?api_key=d950ce535bf05900292bb8ad7bb98628&language=en-US",
       urlSerie:"https://api.themoviedb.org/3/search/tv?api_key=d950ce535bf05900292bb8ad7bb98628&language=en-US",
       filmList: [],
-      serieList:[]
+      seriesList:[]
     }
   },
 
